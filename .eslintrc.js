f680cabd50d2045396f1d7f61538ef98de2c5492
kv3delacruz <kv3delacruz@gmail.com>
2021-07-15 11:00:49 -0600

    init unit testing with vue2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
-};
+   root: true,
+   env: {
+      node: true
+   },
+   plugins: ['vuetify'],
+   extends: [
+      'plugin:vue/recommended',
+      'eslint:recommended',
+      'plugin:prettier/recommended',
+      'prettier/vue'
+   ],
+   rules: {
+      'no-unreachable': 'off',
+      'vue/valid-v-slot': 'off',
+      'vuetify/no-deprecated-classes': 'error',
+      'vue/no-side-effects-in-computed-properties': 'off',
+      'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+   },
+   parserOptions: {
+      parser: 'babel-eslint'
+   }
+}
